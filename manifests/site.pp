f515925610ad02c23b9f7897b88eb25891eebe5c
Marcus Poller <marcus.poller@betadots.de>
2024-07-05 11:58:27 +0200

    no parameter named 'content'

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,12 @@
 file_line{ 'foo':
-  path    => '/tmp/hostnamen.txt',
-  content => $facts['networking']['hostname'],
+  path => '/tmp/hostnamen.txt',
+  line => $facts['networking']['hostname'],
 }
 
 @@file_line{ $facts['networking']['hostname']:
-  path    => '/tmp/hostnamen_collector.txt',
-  content => $facts['networking']['hostname'],
-  tag     => 'kleine_kollekte',
+  path => '/tmp/hostnamen_collector.txt',
+  line => $facts['networking']['hostname'],
+  tag  => 'kleine_kollekte',
 }
 
 File_line <<| tag == 'kleine_kollekte' |>>
