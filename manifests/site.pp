ec3727e25575444b229b8c8e31945d2665db89e9
Marcus Poller <marcus.poller@betadots.de>
2024-07-05 11:26:14 +0200

    Collector

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,3 +2,9 @@ file_line{ 'foo':
   path    => '/tmp/hostnamen.txt',
   content => $facts['networking']['hostname'],
 }
+
+@@file_line{ $facts['networking']['hostname']:
+  path    => '/tmp/hostnamen_collector.txt',
+  content => $facts['networking']['hostname'],
+  tag     => 'kleine_kollekte',
+}
