242c3c79c6fb0beaebe32493cfab8eb8c8e4ae01
Marcus Poller <marcus.poller@betadots.de>
2024-09-05 15:01:10 +0200

    run_plan(noop)

--- modules/plandemo/plans/mynotify.pp
+++ modules/plandemo/plans/mynotify.pp
@@ -1,6 +1,10 @@
 plan plandemo::mynotify {
   notice('find me in the log')
-  $tgt = lookup('myplan', String, 'first', undef)
-  run_plan($tgt)
-  run_plan(undef)
+  $tgt1 = lookup('myplan', String, 'first', undef)
+  run_plan($tgt1)
+  $tgt2 = lookup('does_not_exist', String, 'first', 'plandemo::noop')
+  run_plan($tgt2)
+}
+
+plan plandemo::noop {
 }
