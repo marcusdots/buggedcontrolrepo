c788b8611a78792fa32cb244131523ad27a238f3
Marcus Poller <marcus.poller@betadots.de>
2024-09-20 15:05:41 +0200

    Cyclic dependency

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,35 +1,8 @@
 
-
-file_line{ 'foo':
-  path => '/tmp/hostnamen.txt',
-  line => $facts['networking']['hostname'],
-}
-
-@@file_line{ $facts['networking']['hostname']:
-  path => '/tmp/hostnamen_collector.txt',
-  line => $facts['networking']['hostname'],
-  tag  => 'kleine_kollekte',
-}
-
-File_line <<| tag == 'kleine_kollekte' |>>
-
-$query = "inventory[certname] { }"
-$nodes = puppetdb_query($query).map |$value| { $value["certname"] }
-notify { $nodes: }
 notify { 'foo':
-  message => puppetdb_query($query),
-} 
-
-include foo
-
-node_group { 'All Environments':
-  ensure               => 'present',
-  description          => 'Environment group parent and default',
-  environment          => 'agent-specified',
-  override_environment => 'true',
-  parent               => 'All Nodes',
-  provider             => 'https',
-  rule                 => ['and',
-  ['~', 'name', '.*']],
+  require => Notify['123'],
 }
 
+notify { '123':
+  require => Notify['foo'],
+}
