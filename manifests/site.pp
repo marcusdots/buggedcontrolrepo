83d0bcd2f6b4f67388fb969607b101226bb13d18
Marcus Poller <marcus.poller@betadots.de>
2024-10-15 13:44:17 +0200

    Experiment: Schedule State manipulieren im Dateisystem

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,3 +6,9 @@ vcsrepo { '/root/github.com/bugged_ctl_repo':
   source   => 'https://github.com/marcusdots/buggedcontrolrepo.git',
   revision => 'vcsrepo_interval',
 }
+
+schedule {'myhourly':
+  period => hourly,
+}
+
+notify {'Sometimes you picture me': }
