89dc911504aeb0ff7888b5972ec0c94de43e8424
Marcus Poller <marcus.poller@betadots.de>
2025-03-11 21:18:16 +0100

    Klasse im Katalog

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,1 @@
-node puppet.mpopub1.int.betadots.training {
-  notify{ "Primary": }
-  include chrony
-  include chainloader
-}
-
-node /webserv./ {
-  notify{ "Webserver": }
-  include profile::a
-  include chainloader
-}
-
-node default {
-  notify{ $trusted['certname']: }
-  include chainloader  
-}
+include profile::ccc
