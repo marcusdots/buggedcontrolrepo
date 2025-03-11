4c504534440a64076404fdde211277598ae1d880
Marcus Poller <marcus.poller@betadots.de>
2025-03-11 18:12:23 +0100

    Chainload

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,16 @@
 node puppet.mpopub1.int.betadots.training {
   notify{ "Primary": }
   include chrony
+  include chainloader
 }
 
 node /webserv./ {
   notify{ "Webserver": }
   include profile::a
+  include chainloader
 }
 
 node default {
   notify{ $trusted['certname']: }
+  include chainloader  
 }
