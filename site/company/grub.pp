a758be054a09ce1e96fa28d0dff65702355fac70
Marcus Poller <marcus.poller@betadots.de>
2025-09-29 15:51:21 +0200

    splash options

--- site/company/grub.pp
+++ site/company/grub.pp
@@ -1,5 +1,13 @@
 class company::grub(
+#kernel_parameter { "quiet":
+#  ensure   => present,
+#  bootmode => "recovery",
   Hash $config = {},
 ){
   notify{ 'Grub Config Einstellungen werden geladen':}
+  $config.each |$key, $val| {
+    grub_config { $key:
+      * => $val,
+    }
+  }
 }
